9748f7847dfd55026e188563559af6bc0404bf10
Hugo1307 <hugomiguel_2001_@sapo.pt>
2021-09-08 17:38:26 +0100

    Fix: Added methods to UpdateTokenIndexDetails interface.

--- src/main/java/io/mymetavese/metaapi/api/actions/v2/UpdateTokenIndexDetails.java
+++ src/main/java/io/mymetavese/metaapi/api/actions/v2/UpdateTokenIndexDetails.java
@@ -1,8 +1,24 @@
 package io.mymetavese.metaapi.api.actions.v2;
 
 import io.mymetavese.metaapi.api.RestAction;
+import io.mymetavese.metaapi.api.entities.v2.WalletIndex;
 import io.mymetavese.metaapi.api.entities.v2.WalletItem;
+import io.mymetavese.metaapi.requests.actions.v2.UpdateTokenIndexDetailsImpl;
 
 public interface UpdateTokenIndexDetails extends RestAction<WalletItem> {
+
+    /**
+     * Update the name of a {@link WalletIndex}.
+     * @param name The new name.
+     * @return The Action to continue the editing session.
+     */
+    UpdateTokenIndexDetailsImpl setName(String name);
+
+    /**
+     * Update the description of a {@link WalletIndex}.
+     * @param description The new description.
+     * @return The Action to continue the editing session.
+     */
+    UpdateTokenIndexDetailsImpl setDescription(String description);
 
 }
