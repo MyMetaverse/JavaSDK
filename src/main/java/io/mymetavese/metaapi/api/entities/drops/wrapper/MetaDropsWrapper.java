e2190d07a28f6efb0f5663964e8238aa23993762
Hugo1307 <hugomiguel_2001_@sapo.pt>
2023-04-06 13:13:57 +0100

    fix: Added MetaDropsWrapper methods to interface.

--- src/main/java/io/mymetavese/metaapi/api/entities/drops/wrapper/MetaDropsWrapper.java
+++ src/main/java/io/mymetavese/metaapi/api/entities/drops/wrapper/MetaDropsWrapper.java
@@ -1,6 +1,21 @@
 package io.mymetavese.metaapi.api.entities.drops.wrapper;
 
 import io.mymetavese.metaapi.api.ApiEntity;
+import io.mymetavese.metaapi.api.actions.drops.ConsumeDropAction;
+import io.mymetavese.metaapi.api.actions.drops.GetAllDropsAction;
+import io.mymetavese.metaapi.api.actions.drops.GetDropAction;
+import io.mymetavese.metaapi.api.entities.drops.requirements.DropEntryRequirement;
+import io.mymetavese.metaapi.api.entities.v2.GameEntity;
+
+import java.util.List;
 
 public interface MetaDropsWrapper extends ApiEntity {
+
+    GetAllDropsAction getAllDrops();
+
+    GetDropAction getDropById(String dropId);
+
+    ConsumeDropAction consumeDrop(String dropId, GameEntity dropReceiver, List<DropEntryRequirement> dropEntryRequirements);
+
+
 }
